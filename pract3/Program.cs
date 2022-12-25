af42781f9a97ecda33015a24a0a0aa1ab0e91b21
git_pract <noxi1992@mail.ru>
2022-12-25 12:56:12 +0300

    добавили коммит

---
+++ pract3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдает, 
+// является ли оно четным
+
+Console.WriteLine("Введите число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if ((x % 2) == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
